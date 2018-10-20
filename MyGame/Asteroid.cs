2d998414a9efb9483c92588e431cfa2e257d52ee
DeeZyVrn <deezyvrn@gmail.com>
2018-10-20 15:57:03 +0300

    WPF

---
+++ MyGame/Asteroid.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace MyGame
+{
+    class Asteroid:BaseObject, ICloneable, IComparable
+    {
+        public int Power { get; set; } = 3;
+
+        public Asteroid(Point pos, Point dir, Size size):base(pos,dir,size)
+        {
+            Power = 1;
+        }
+
+        public object Clone()
+        {
+            Asteroid asteroid = new Asteroid(new Point(Pos.X, Pos.Y), new Point(Dir.X, Dir.Y), new Size(Size.Width, Size.Height))
+            { Power = Power };
+            return asteroid;
+        }
+
+        public override void Draw()
+        {
+            Game.Buffer.Graphics.FillEllipse(Brushes.White, Pos.X, Pos.Y, Size.Width, Size.Height);
+        }
+
+        int IComparable.CompareTo(object obj)
+        {
+            if (obj is Asteroid temp)
+            {
+                if (Power > temp.Power)
+                    return 1;
+                if (Power < temp.Power)
+                    return -1;
+                else return 0;
+            }
+            throw new ArgumentException("Parameter is not an Asteroid!");
+        }
+
+        public override void Update()
+        {
+            //объекты меняют направление при достижении края формы
+            Pos.X += Dir.X;
+            Pos.Y += Dir.Y;
+            if (Pos.X < 0 || Pos.X > Game.Width) { Dir.X = -Dir.X; }
+            else if (Pos.Y < 0 || Pos.Y > Game.Height) { Dir.Y = -Dir.Y; }
+        }
+    }
+}
---
+++ MyGame/BaseObject.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace MyGame
+{
+    /// <summary>
+    /// Базовый объект
+    /// </summary>
+    abstract class BaseObject: ICollision
+    {
+        protected Point Pos;
+        protected Point Dir;
+        protected Size Size;
+
+        protected Image img;
+
+        public delegate void Message();
+
+        protected BaseObject(Point pos, Point dir, Size size)
+        {
+            Pos = pos;
+            Dir = dir;
+            Size = size;
+        }
+
+        /// <summary>
+        /// форма базового объекта
+        /// </summary>
+        public abstract void Draw();
+
+        /// <summary>
+        /// начальное поведение объектов
+        /// </summary>
+        public abstract void Update();
+
+        /// <summary>
+        /// Метод вставки картинки из файла
+        /// </summary>
+        /// <param name="fileName"></param>
+        protected void LoadImage(string fileName)
+        {
+            img = Image.FromFile(fileName);
+        }
+
+        public bool Collision(ICollision o) => o.Rect.IntersectsWith(this.Rect); //обнаружение пересечения объектов
+        public Rectangle Rect => new Rectangle(Pos, Size);
+    }
+}
