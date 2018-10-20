e631d37339eefeab76ab97f2353421373f782234
DeeZyVrn <deezyvrn@gmail.com>
2018-10-20 15:57:03 +0300

    Initial commit

---
+++ WindowsFormsApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MyGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form form = new Form();
+            {
+                var Width = Screen.PrimaryScreen.Bounds.Width;
+                var Height = Screen.PrimaryScreen.Bounds.Height;
+            }
+            Game.Init(form);
+            form.Show();
+            Game.Load();
+            Game.Draw();
+            Application.Run(form);
+        }
+    }
+}
